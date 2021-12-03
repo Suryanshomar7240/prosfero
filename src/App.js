04b6ad346cd3d752bfb520f4f52e46f72e36e0b6
Suryansh Omar <suryu7240@gmail.com>
2021-12-03 18:26:53 +0530

    intital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './header/Nav';
+// import {Het} from './header/Het';
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      {/* <Het /> */}
+      </div>
+  );
+}
+
+export default App;
