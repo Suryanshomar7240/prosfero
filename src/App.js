0e26d9f2cda8d395f9554af3046218871cbf20e6
Suryansh Omar <suryu7240@gmail.com>
2021-12-06 01:02:05 +0530

    file directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// import logo from './logo.svg';
-import './App.css';
-import Nav from './header/Nav';
-// import {Het} from './header/Het';
+// import './App.css';
+import Nav from './components/header/Nav';
+
 function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <Het /> */}
       </div>
   );
 }
---
+++ src/components/header/Nav.js
@@ -0,0 +1,19 @@
+// "" tro torp
+import { useEffect } from "react";
+import "./Nav.css";
+const Nav = () => {
+	return(
+		<>
+		<nav>
+			<ul className="nav-list">
+				<li className="nav-item"><a href="#">Home</a></li>
+				<li className="nav-item"><a href="#">Exploer</a></li>
+				<li className="nav-item"><a href="#">Contact us</a></li>
+				<li className="nav-item"><a href="#">Login/SignUp</a></li>
+			</ul>
+		</nav>
+
+		</>
+	)
+}
+export default Nav;	
