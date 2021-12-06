bce39cecc755dd3722fbaab18de588ef6a07ff2d
Suryansh Omar <suryu7240@gmail.com>
2021-12-06 18:16:08 +0530

    logo+headerissues

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -8,35 +8,24 @@ import "./Nav.css";
 // import { GiHamburgerMenu } from "react-icons/gi";
 import {MdOutlineAccountCircle} from "react-icons/md";
 import {GiHamburgerMenu} from "react-icons/gi";
-import React,{ useEffect } from "react";
+import React from "react";
 // import React from "react";
 // import { NavLink } from "react-router-dom";
-
+import logo from "../../assets/logo.png";
 const Nav = () => {
-  useEffect(() => {
-    console.log(window.innerWidth);
-    if(window.innerWidth > 768){
-      document.querySelector(".hamburger-menu").classList.add("display_none");
-    }
-    else
-    {
-      document.querySelector(".nav-bar").classList.add("display_none");
-      // console.log(document.querySelector(".nav-bar"));
-    }
-  },[]);
   return (
-    <>
+    <div className="navigation">
       <nav>
-        <div className="nav_org_logo">Prosfero</div>
+        <div className="nav_org_logo"><img src={logo} alt="logo"/></div>
         <ul className="nav-bar">
-          <li className="nav-item"><a href="www.google.com">Home</a></li>
+          <li className="nav-item"><a href="#home">Home</a></li>
           <li className="nav-item"><a href="www.google.com">Explore</a></li>
           <li className="nav-item"><a href="www.google.com">Create<small> Donation</small></a></li>
           <li className="nav-item"><a href="www.google.com">Login/Signup <MdOutlineAccountCircle /></a></li>
         </ul>
         <div className="hamburger-menu"><GiHamburgerMenu /></div>
       </nav>
-    </>
+    </div>
   );
 };
 
