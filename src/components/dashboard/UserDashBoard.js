98b5d09fbbfedd9ace28a32c1dd0ec7e31ecac2e
Suryansh Omar <suryu7240@gmail.com>
2021-12-15 01:40:26 +0530

    changes in user pannel

--- src/components/dashboard/UserDashBoard.js
+++ src/components/dashboard/UserDashBoard.js
@@ -1,83 +1,34 @@
 import React from "react";
 import "./UserDashBoard.css";
-import DonationCard from "../donationCards/DonationCard";
-import { useState } from "react";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-import dummy_data from "./dummy_data";
+// import donation from 'https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg';
+// import DonationCard from "../donationCards/DonationCard";
+// import { useState } from "react";
+// import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+// import dummy_data from "./dummy_data";
 const UserDashBoard = () => {
-  const size=5;
-  const [carasoulState, SetcarasoulState] = useState(0);
-  const HandleNext = () => {
-    if (carasoulState === size-1) {
-      SetcarasoulState(0);
-    } else {
-      SetcarasoulState(carasoulState + 1);
-    }
-  };
-  const HandlePrev = () => {
-    if (carasoulState === 0) {
-      SetcarasoulState(size-1);
-    } else {
-      SetcarasoulState(carasoulState - 1);
-    }
-  };
   return (
     <div className="dashboard">
       <div className="User_name">Hello,Suryansh :)</div>
       <div className="User-fundraiser">
-        <div className="User_funds_head">Your running Fundraisers</div>
-        <div className="User_funds_carasoul">
-          <div className="User_funds_next_arrow" onClick={HandleNext}>
-            <AiOutlineLeft />
-          </div>
-          <div className="User_funds_donationCards">
-            {dummy_data.map((item, index) => {
-              if (index === (carasoulState - 1))
-                return (
-                  <DonationCard
-                    key={index}
-                    imgLink={item.imgLink}
-                    userName={item.userName}
-                    userImg={item.userImg}
-                    orgName={item.orgName}
-                    progress={item.progress}
-                    required={item.required}
-                    width='secondary'
-                  />
-                );
-              else if(carasoulState===0)
-                return (<div></div>);
-              if (index === carasoulState)
-                return (
-                  <DonationCard
-                    imgLink={item.imgLink}
-                    userName={item.userName}
-                    userImg={item.userImg}
-                    orgName={item.orgName}
-                    progress={item.progress}
-                    required={item.required}
-                    width='primary'
-                  />
-                );
-              else if (
-                index === (carasoulState + 1)
-              )
-                return (
-                  <DonationCard
-                    imgLink={item.imgLink}
-                    userName={item.userName}
-                    userImg={item.userImg}
-                    orgName={item.orgName}
-                    progress={item.progress}
-                    required={item.required}
-                    width='secondary'
-                  />
-                );
-              else return <div></div>;
-            })}
-          </div>
-          <div className="User_funds_prev_arrow" onClick={HandlePrev}>
-            <AiOutlineRight />
+        <div className="Dashboard-navigation">
+          <ul className="navigation-list">
+            <li className="navigation-items"><a href="/user">Overview</a></li>
+            <li className="navigation-items"><a href="/user">Fundraisers</a></li>
+            <li className="navigation-items"><a href="/user">Donations</a></li>
+            <li className="navigation-items"><a href="/user">Profile Update</a></li>
+          </ul>
+        </div>
+        <div className="Dashboard-content">
+          <div className="User_funds_head">Yours Recent Running Fundraisers</div>
+          <div className="User_recent_fund_card">
+            <div className="Card_left">
+              <img src="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="fundraiser-Image" />
+              <div className='userDetail'>
+                <img src="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="Joshraj" />
+                <span className='userName'>By Suryansh</span>
+              </div>
+            </div>
+            <div className="Card_right"></div>
           </div>
         </div>
       </div>
