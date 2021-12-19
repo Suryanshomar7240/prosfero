6ae8bffe1ff38c093f7c633fad595a9269f3e3b5
Suryansh Omar <suryu7240@gmail.com>
2021-12-19 18:33:26 +0530

    Little changes in user Panel

--- src/components/dashboard/UserDashBoard.js
+++ src/components/dashboard/UserDashBoard.js
@@ -1,66 +1,116 @@
 import React from "react";
 import "./UserDashBoard.css";
-// import donation from 'https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg';
-// import DonationCard from "../donationCards/DonationCard";
-// import { useState } from "react";
-// import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-// import dummy_data from "./dummy_data";
+import Carousel from "react-elastic-carousel";
+import Navigation from "./navigation/navigation";
+import Fundraiser from "./fundraiser/Fundraiser";
+import Donation from "./donations/donation";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 3 },
+];
+
 const UserDashBoard = () => {
   return (
     <div className="dashboard">
       <div className="User_name">Hello,Suryansh :)</div>
       <div className="User-fundraiser">
         <div className="Dashboard-navigation">
-          <ul className="navigation-list">
-            <li className="navigation-items">
-              <a href="/user/overview">Overview</a>
-            </li>
-            <li className="navigation-items">
-              <a href="/user/fundraiser">Fundraisers</a>
-            </li>
-            <li className="navigation-items">
-              <a href="/user">Donations</a>
-            </li>
-            <li className="navigation-items">
-              <a href="/user">Profile Update</a>
-            </li>
-          </ul>
+          <Navigation />
         </div>
         <div className="Dashboard-content">
           <div className="User_funds_head">
             Yours Recent Running Fundraisers
           </div>
-          <div className="User_recent_fund_card">
-            <div className="Card_left">
-              <img
-                src="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
-                alt="donation"
+            <Fundraiser imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                width="primary"
+                FundName="For Medical Camp"
+                />
+          <div className="User_donations">
+            <div className="User_funds_head">Your Top Donations</div>
+            <Carousel breakPoints={breakPoints}>
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                AmtDonated="1000"
               />
-              <div className="userDetail"></div>
-            </div>
-            <div className="Card_right">
-              <div><img
-                src="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
-                alt="Joshraj"
+
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                AmtDonated="100"
+                width="primary"
               />
-              <span className="userName">By Suryansh</span></div>
-              <div className="fund_Target">Target : <span className="money">10000</span></div>
-              <div className="fund_amount">Target acheived : <span className="money">5000</span></div>
-              <div className="fund_progress">Progress bar :</div>
-              <div class="progress">
-                <div
-                  class="progress-bar"
-                  role="progressbar"
-                  style={{
-                    width: (5000 / 10000) * 100 + "%",
-                    backgroundColor: "#14fc52",
-                  }}
-                  aria-valuenow="25"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                ><span style={{float:'right'}}>{(500/1000)*100}%</span></div>
-              </div>
-            </div>
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                AmtDonated="100"
+                required="100000"
+              />
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                AmtDonated="100"
+              />
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                AmtDonated="100"
+              />
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                AmtDonated="100"
+              />
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                AmtDonated="100"
+              />
+              <Donation
+                imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                AmtDonated="100"
+              />
+            </Carousel>
           </div>
         </div>
       </div>
