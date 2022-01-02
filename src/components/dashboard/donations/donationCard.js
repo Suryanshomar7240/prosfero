9991f10bf23c952589bb60764743fb0575ec991f
Suryanshomar7240 <suryu7240@gmail.com>
2022-01-02 16:35:34 +0530

    dashboard update

---
+++ src/components/dashboard/donations/donationCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+const Donation= (props) => {
+  return (
+    <>
+      <div className='card_wrapper user_donation'>
+        <div className='img_container'>
+          <img src={props.imgLink} alt='donation' />{' '}
+        </div>
+        <div className='card_title'>{props.orgName}</div>
+        <div className='userDetail'>
+          <img src={props.userImg} alt={props.userName} />
+          <span className='userName'>By {props.userName}</span>
+        </div>
+        <div className="amt_user_donated">Your Donation : <span className='money'>{props.AmtDonated}₹</span></div>
+        <div className="percentage_user_donated">Your Percentage Donation : <span className='money'> {(props.AmtDonated / props.required) * 100}%</span></div>
+        <span className='collection'>
+          <span className='money'>₹{props.progress}</span> raised out of{' '}
+          <span className='money'>₹{props.required}</span>
+        </span>
+        <div class='progress'>
+          <div
+            class='progress-bar'
+            role='progressbar'
+            style={{ width: (props.progress / props.required) * 100 + '%' , backgroundColor: "#318CE7"}}
+            aria-valuenow='25'
+            aria-valuemin='0'
+            aria-valuemax='100'
+          ></div>
+        </div>
+        <button className="donate_button">Donate More</button>
+      </div>
+    </>
+  );
+};
+
+export default Donation;
---
+++ src/components/dashboard/fundraiser/fundraiser.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Fundraiser from './FundraiserCard';
+const Fundraisers = () => {
+    return (
+        <div className='fundraiser-grid'>
+            <Fundraiser imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                FundName="For Medical Camp"
+            /><Fundraiser imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                FundName="For Medical Camp"
+            /><Fundraiser imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                FundName="For Medical Camp"
+            /><Fundraiser imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                FundName="For Medical Camp"
+            /><Fundraiser imgLink="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userImg="https://miro.medium.com/max/1400/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+                userName="yash"
+                orgName="Gareeb Foundation"
+                progress="90000"
+                required="100000"
+                FundName="For Medical Camp"
+            />
+        </div>
+    );
+}
+
+export default Fundraisers;
