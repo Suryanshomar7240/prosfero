65a479bed6bc174c9351ab0d7f9ca96c5bf50002
Suryanshomar7240 <suryu7240@gmail.com>
2022-01-08 15:00:03 +0530

    Added mail service in Contact form

--- frontend/src/components/contactPage/Contact.js
+++ frontend/src/components/contactPage/Contact.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './contact.css';
-
+import { send,init} from 'emailjs-com';
 const Contact = () => {
+  const [data,SetData]=useState({
+    sender:'',
+    email:'',
+    message:''
+  })
+  init('user_36SNYbq8KaIwjEzuiv63F');
+ let name, value;
+  const handleInputs = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    SetData({ ...data, [name]: value });
+  };
+
+  const sendmail = (e) => {
+    e.preventDefault();
+
+    send(
+      'service_4nhy15e',
+      'template_dnrtv93',
+      data,'user_36SNYbq8KaIwjEzuiv63F'
+    )
+      .then((response) => {
+        window.alert("Message sent successfully to Prosfero Team")
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
   return (
     <div>
       <section id='contact'>
@@ -13,17 +41,17 @@ const Contact = () => {
             <form>
               <div className='form-item'>
                 <label>Name:</label>
-                <input type='text' name='sender' required />
+                <input type='text' name='sender' onChange={handleInputs}required />
               </div>
               <div className='form-item'>
                 <label>Email:</label>
-                <input type='text' name='email' required />
+                <input type='text' name='email' onChange={handleInputs} required />
               </div>
               <div className='form-item'>
                 <label>Message:</label>
-                <textarea className='' name='message' required></textarea>
+                <textarea className='' name='message'  onChange={handleInputs}required></textarea>
               </div>
-              <button className='submit-btn'>Send</button>
+              <div className='submit-btn' onClick={sendmail}>Send</div>
             </form>
           </div>
         </div>
